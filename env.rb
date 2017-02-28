9ad32892116812d58b61e963ceac4912f17dd0fc
Lucas Vidal <lvidal@twilio.com>
2017-02-28 16:08:20 -0500

    Sample Backend Improvements
    Removed rest-client dependency since it breaks with ruby 2.4. Using net/http
    
    Improved documentation and forwarded errors from Authy API

---
+++ env.rb
@@ -0,0 +1,6 @@
+# The api_key can be obtained by logging into the Authy dashboard and selecting the app for 
+# which you are authenticating users. 
+#
+# https://dashboard.authy.com/
+
+ENV["AUTHY_API_KEY"] = "YOUR_API_KEY"
