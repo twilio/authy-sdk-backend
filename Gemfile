705971da5d3d5b7667a0d8e8c1faaa668312eac5
Julian Suarez <jsuarez@twilio.com>
2017-04-19 16:56:28 -0500

    Added endpoint to create JWT

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-param'
+gem 'jwt'
 
 group :development do
   gem 'pry'
--- backend.rb
+++ backend.rb
@@ -72,3 +72,17 @@ post "/registration" do
   end
 end
 
+post "/verify/token" do
+  param :phone_number, String, required: true
+
+  payload = {
+    app_id: ENV["APP_ID"],
+    phone_number: params[:phone_number],
+    iat: Time.now.to_i
+  }
+
+  jwt_token = JWT.encode(payload, ENV["AUTHY_API_KEY"], "HS256")
+
+  respond_with status: 200, body: {jwt_token: jwt_token}
+end
+
